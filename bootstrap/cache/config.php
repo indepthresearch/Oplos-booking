de622700059f0bba7b5f7f1f7a902c4683454e1a
indepthresearch <42144242+indepthresearch@users.noreply.github.com>
2018-09-20 07:45:25 +0300

    Add files via upload
    bootstrap

---
+++ bootstrap/cache/config.php
@@ -0,0 +1,692 @@
+<?php return array (
+  'app' => 
+  array (
+    'name' => 'Laravel',
+    'env' => 'local',
+    'debug' => true,
+    'url' => 'http://localhost',
+    'timezone' => 'UTC',
+    'locale' => 'en',
+    'fallback_locale' => 'en',
+    'key' => 'base64:+NTvclkbnzPQN4o4uToHdCK8HM9x2h43w4ZhA5xuwjw=',
+    'cipher' => 'AES-256-CBC',
+    'providers' => 
+    array (
+      0 => 'Illuminate\\Auth\\AuthServiceProvider',
+      1 => 'Illuminate\\Broadcasting\\BroadcastServiceProvider',
+      2 => 'Illuminate\\Bus\\BusServiceProvider',
+      3 => 'Illuminate\\Cache\\CacheServiceProvider',
+      4 => 'Illuminate\\Foundation\\Providers\\ConsoleSupportServiceProvider',
+      5 => 'Illuminate\\Cookie\\CookieServiceProvider',
+      6 => 'Illuminate\\Database\\DatabaseServiceProvider',
+      7 => 'Illuminate\\Encryption\\EncryptionServiceProvider',
+      8 => 'Illuminate\\Filesystem\\FilesystemServiceProvider',
+      9 => 'Illuminate\\Foundation\\Providers\\FoundationServiceProvider',
+      10 => 'Illuminate\\Hashing\\HashServiceProvider',
+      11 => 'Illuminate\\Mail\\MailServiceProvider',
+      12 => 'Illuminate\\Notifications\\NotificationServiceProvider',
+      13 => 'Illuminate\\Pagination\\PaginationServiceProvider',
+      14 => 'Illuminate\\Pipeline\\PipelineServiceProvider',
+      15 => 'Illuminate\\Queue\\QueueServiceProvider',
+      16 => 'Illuminate\\Redis\\RedisServiceProvider',
+      17 => 'Illuminate\\Auth\\Passwords\\PasswordResetServiceProvider',
+      18 => 'Illuminate\\Session\\SessionServiceProvider',
+      19 => 'Illuminate\\Translation\\TranslationServiceProvider',
+      20 => 'Illuminate\\Validation\\ValidationServiceProvider',
+      21 => 'Illuminate\\View\\ViewServiceProvider',
+      22 => 'Laravel\\Scout\\ScoutServiceProvider',
+      23 => 'TCG\\Voyager\\VoyagerServiceProvider',
+      24 => 'Safaricom\\Mpesa\\MpesaServiceProvider',
+      25 => 'App\\Providers\\AppServiceProvider',
+      26 => 'App\\Providers\\AuthServiceProvider',
+      27 => 'App\\Providers\\EventServiceProvider',
+      28 => 'App\\Providers\\RouteServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'App' => 'Illuminate\\Support\\Facades\\App',
+      'Artisan' => 'Illuminate\\Support\\Facades\\Artisan',
+      'Auth' => 'Illuminate\\Support\\Facades\\Auth',
+      'Blade' => 'Illuminate\\Support\\Facades\\Blade',
+      'Broadcast' => 'Illuminate\\Support\\Facades\\Broadcast',
+      'Bus' => 'Illuminate\\Support\\Facades\\Bus',
+      'Cache' => 'Illuminate\\Support\\Facades\\Cache',
+      'Config' => 'Illuminate\\Support\\Facades\\Config',
+      'Cookie' => 'Illuminate\\Support\\Facades\\Cookie',
+      'Crypt' => 'Illuminate\\Support\\Facades\\Crypt',
+      'DB' => 'Illuminate\\Support\\Facades\\DB',
+      'Eloquent' => 'Illuminate\\Database\\Eloquent\\Model',
+      'Event' => 'Illuminate\\Support\\Facades\\Event',
+      'File' => 'Illuminate\\Support\\Facades\\File',
+      'Gate' => 'Illuminate\\Support\\Facades\\Gate',
+      'Hash' => 'Illuminate\\Support\\Facades\\Hash',
+      'Lang' => 'Illuminate\\Support\\Facades\\Lang',
+      'Log' => 'Illuminate\\Support\\Facades\\Log',
+      'Mail' => 'Illuminate\\Support\\Facades\\Mail',
+      'Notification' => 'Illuminate\\Support\\Facades\\Notification',
+      'Password' => 'Illuminate\\Support\\Facades\\Password',
+      'Queue' => 'Illuminate\\Support\\Facades\\Queue',
+      'Redirect' => 'Illuminate\\Support\\Facades\\Redirect',
+      'Redis' => 'Illuminate\\Support\\Facades\\Redis',
+      'Request' => 'Illuminate\\Support\\Facades\\Request',
+      'Response' => 'Illuminate\\Support\\Facades\\Response',
+      'Route' => 'Illuminate\\Support\\Facades\\Route',
+      'Schema' => 'Illuminate\\Support\\Facades\\Schema',
+      'Session' => 'Illuminate\\Support\\Facades\\Session',
+      'Storage' => 'Illuminate\\Support\\Facades\\Storage',
+      'URL' => 'Illuminate\\Support\\Facades\\URL',
+      'Validator' => 'Illuminate\\Support\\Facades\\Validator',
+      'View' => 'Illuminate\\Support\\Facades\\View',
+      'Mpesa' => 'Safaricom\\Mpesa\\MpesaServiceProvider',
+    ),
+  ),
+  'auth' => 
+  array (
+    'defaults' => 
+    array (
+      'guard' => 'web',
+      'passwords' => 'users',
+    ),
+    'guards' => 
+    array (
+      'web' => 
+      array (
+        'driver' => 'session',
+        'provider' => 'users',
+      ),
+      'api' => 
+      array (
+        'driver' => 'token',
+        'provider' => 'users',
+      ),
+    ),
+    'providers' => 
+    array (
+      'users' => 
+      array (
+        'driver' => 'eloquent',
+        'model' => 'App\\User',
+      ),
+    ),
+    'passwords' => 
+    array (
+      'users' => 
+      array (
+        'provider' => 'users',
+        'table' => 'password_resets',
+        'expire' => 60,
+      ),
+    ),
+  ),
+  'broadcasting' => 
+  array (
+    'default' => 'log',
+    'connections' => 
+    array (
+      'pusher' => 
+      array (
+        'driver' => 'pusher',
+        'key' => '',
+        'secret' => '',
+        'app_id' => '',
+        'options' => 
+        array (
+          'cluster' => 'mt1',
+          'encrypted' => true,
+        ),
+      ),
+      'redis' => 
+      array (
+        'driver' => 'redis',
+        'connection' => 'default',
+      ),
+      'log' => 
+      array (
+        'driver' => 'log',
+      ),
+      'null' => 
+      array (
+        'driver' => 'null',
+      ),
+    ),
+  ),
+  'cache' => 
+  array (
+    'default' => 'file',
+    'stores' => 
+    array (
+      'apc' => 
+      array (
+        'driver' => 'apc',
+      ),
+      'array' => 
+      array (
+        'driver' => 'array',
+      ),
+      'database' => 
+      array (
+        'driver' => 'database',
+        'table' => 'cache',
+        'connection' => NULL,
+      ),
+      'file' => 
+      array (
+        'driver' => 'file',
+        'path' => 'C:\\xampp\\htdocs\\bookings\\Bookings\\storage\\framework/cache/data',
+      ),
+      'memcached' => 
+      array (
+        'driver' => 'memcached',
+        'persistent_id' => NULL,
+        'sasl' => 
+        array (
+          0 => NULL,
+          1 => NULL,
+        ),
+        'options' => 
+        array (
+        ),
+        'servers' => 
+        array (
+          0 => 
+          array (
+            'host' => '127.0.0.1',
+            'port' => 11211,
+            'weight' => 100,
+          ),
+        ),
+      ),
+      'redis' => 
+      array (
+        'driver' => 'redis',
+        'connection' => 'default',
+      ),
+    ),
+    'prefix' => 'laravel_cache',
+  ),
+  'cart' => 
+  array (
+    'tax' => 16,
+    'database' => 
+    array (
+      'connection' => NULL,
+      'table' => 'shoppingcart',
+    ),
+    'destroy_on_logout' => false,
+    'format' => 
+    array (
+      'decimals' => 2,
+      'decimal_point' => '.',
+      'thousand_seperator' => ',',
+    ),
+  ),
+  'database' => 
+  array (
+    'default' => 'mysql',
+    'connections' => 
+    array (
+      'sqlite' => 
+      array (
+        'driver' => 'sqlite',
+        'database' => 'bookings.com',
+        'prefix' => '',
+      ),
+      'mysql' => 
+      array (
+        'driver' => 'mysql',
+        'host' => '127.0.0.1',
+        'port' => '3306',
+        'database' => 'bookings.com',
+        'username' => 'root',
+        'password' => '',
+        'unix_socket' => '',
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'prefix' => '',
+        'strict' => false,
+        'engine' => NULL,
+      ),
+      'pgsql' => 
+      array (
+        'driver' => 'pgsql',
+        'host' => '127.0.0.1',
+        'port' => '3306',
+        'database' => 'bookings.com',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8mb4',
+        'prefix' => '',
+        'schema' => 'public',
+        'sslmode' => 'prefer',
+      ),
+      'sqlsrv' => 
+      array (
+        'driver' => 'sqlsrv',
+        'host' => '127.0.0.1',
+        'port' => '3306',
+        'database' => 'bookings.com',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8mb4',
+        'prefix' => '',
+      ),
+    ),
+    'migrations' => 'migrations',
+    'redis' => 
+    array (
+      'client' => 'predis',
+      'default' => 
+      array (
+        'host' => '127.0.0.1',
+        'password' => NULL,
+        'port' => '6379',
+        'database' => 0,
+      ),
+    ),
+  ),
+  'filesystems' => 
+  array (
+    'default' => 'local',
+    'cloud' => 's3',
+    'disks' => 
+    array (
+      'local' => 
+      array (
+        'driver' => 'local',
+        'root' => 'C:\\xampp\\htdocs\\bookings\\Bookings\\storage\\app',
+      ),
+      'public' => 
+      array (
+        'driver' => 'local',
+        'root' => 'C:\\xampp\\htdocs\\bookings\\Bookings\\storage\\app/public',
+        'url' => 'http://localhost/storage',
+        'visibility' => 'public',
+      ),
+      's3' => 
+      array (
+        'driver' => 's3',
+        'key' => NULL,
+        'secret' => NULL,
+        'region' => NULL,
+        'bucket' => NULL,
+        'url' => NULL,
+      ),
+    ),
+  ),
+  'hashing' => 
+  array (
+    'driver' => 'bcrypt',
+    'bcrypt' => 
+    array (
+      'rounds' => 10,
+    ),
+    'argon' => 
+    array (
+      'memory' => 1024,
+      'threads' => 2,
+      'time' => 2,
+    ),
+  ),
+  'hooks' => 
+  array (
+    'enabled' => true,
+  ),
+  'image' => 
+  array (
+    'driver' => 'gd',
+  ),
+  'logging' => 
+  array (
+    'default' => 'stack',
+    'channels' => 
+    array (
+      'stack' => 
+      array (
+        'driver' => 'stack',
+        'channels' => 
+        array (
+          0 => 'single',
+        ),
+      ),
+      'single' => 
+      array (
+        'driver' => 'single',
+        'path' => 'C:\\xampp\\htdocs\\bookings\\Bookings\\storage\\logs/laravel.log',
+        'level' => 'debug',
+      ),
+      'daily' => 
+      array (
+        'driver' => 'daily',
+        'path' => 'C:\\xampp\\htdocs\\bookings\\Bookings\\storage\\logs/laravel.log',
+        'level' => 'debug',
+        'days' => 7,
+      ),
+      'slack' => 
+      array (
+        'driver' => 'slack',
+        'url' => NULL,
+        'username' => 'Laravel Log',
+        'emoji' => ':boom:',
+        'level' => 'critical',
+      ),
+      'stderr' => 
+      array (
+        'driver' => 'monolog',
+        'handler' => 'Monolog\\Handler\\StreamHandler',
+        'with' => 
+        array (
+          'stream' => 'php://stderr',
+        ),
+      ),
+      'syslog' => 
+      array (
+        'driver' => 'syslog',
+        'level' => 'debug',
+      ),
+      'errorlog' => 
+      array (
+        'driver' => 'errorlog',
+        'level' => 'debug',
+      ),
+    ),
+  ),
+  'mail' => 
+  array (
+    'driver' => 'smtp',
+    'host' => 'smtp.mailtrap.io',
+    'port' => '465',
+    'from' => 
+    array (
+      'address' => 'admin@oplosbooking.com',
+      'name' => 'admin',
+    ),
+    'encryption' => NULL,
+    'username' => 'd9f8ae6af19d36',
+    'password' => '62e40c0b713864',
+    'sendmail' => '/usr/sbin/sendmail -bs',
+    'markdown' => 
+    array (
+      'theme' => 'default',
+      'paths' => 
+      array (
+        0 => 'C:\\xampp\\htdocs\\bookings\\Bookings\\resources\\views/vendor/mail',
+      ),
+    ),
+  ),
+  'queue' => 
+  array (
+    'default' => 'sync',
+    'connections' => 
+    array (
+      'sync' => 
+      array (
+        'driver' => 'sync',
+      ),
+      'database' => 
+      array (
+        'driver' => 'database',
+        'table' => 'jobs',
+        'queue' => 'default',
+        'retry_after' => 90,
+      ),
+      'beanstalkd' => 
+      array (
+        'driver' => 'beanstalkd',
+        'host' => 'localhost',
+        'queue' => 'default',
+        'retry_after' => 90,
+      ),
+      'sqs' => 
+      array (
+        'driver' => 'sqs',
+        'key' => 'your-public-key',
+        'secret' => 'your-secret-key',
+        'prefix' => 'https://sqs.us-east-1.amazonaws.com/your-account-id',
+        'queue' => 'your-queue-name',
+        'region' => 'us-east-1',
+      ),
+      'redis' => 
+      array (
+        'driver' => 'redis',
+        'connection' => 'default',
+        'queue' => 'default',
+        'retry_after' => 90,
+        'block_for' => NULL,
+      ),
+    ),
+    'failed' => 
+    array (
+      'database' => 'mysql',
+      'table' => 'failed_jobs',
+    ),
+  ),
+  'scout' => 
+  array (
+    'driver' => 'algolia',
+    'prefix' => '',
+    'queue' => true,
+    'chunk' => 
+    array (
+      'searchable' => 500,
+      'unsearchable' => 500,
+    ),
+    'soft_delete' => false,
+    'algolia' => 
+    array (
+      'id' => 'ZR61N5O4S2',
+      'secret' => 'c5653f38f81693c77a302b86d26b5f64',
+    ),
+  ),
+  'services' => 
+  array (
+    'mailgun' => 
+    array (
+      'domain' => NULL,
+      'secret' => NULL,
+    ),
+    'ses' => 
+    array (
+      'key' => NULL,
+      'secret' => NULL,
+      'region' => 'us-east-1',
+    ),
+    'sparkpost' => 
+    array (
+      'secret' => NULL,
+    ),
+    'stripe' => 
+    array (
+      'secret' => 'sk_test_BzfLz9d0ubE5jba7Dh3bwr62',
+      'version' => '2015-01-11',
+    ),
+  ),
+  'session' => 
+  array (
+    'driver' => 'file',
+    'lifetime' => '120',
+    'expire_on_close' => false,
+    'encrypt' => false,
+    'files' => 'C:\\xampp\\htdocs\\bookings\\Bookings\\storage\\framework/sessions',
+    'connection' => NULL,
+    'table' => 'sessions',
+    'store' => NULL,
+    'lottery' => 
+    array (
+      0 => 2,
+      1 => 100,
+    ),
+    'cookie' => 'laravel_session',
+    'path' => '/',
+    'domain' => NULL,
+    'secure' => false,
+    'http_only' => true,
+    'same_site' => NULL,
+  ),
+  'view' => 
+  array (
+    'paths' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\bookings\\Bookings\\resources\\views',
+    ),
+    'compiled' => 'C:\\xampp\\htdocs\\bookings\\Bookings\\storage\\framework\\views',
+  ),
+  'voyager' => 
+  array (
+    'user' => 
+    array (
+      'add_default_role_on_register' => true,
+      'default_role' => 'user',
+      'namespace' => NULL,
+      'default_avatar' => 'users/default.png',
+      'redirect' => '/admin',
+    ),
+    'controllers' => 
+    array (
+      'namespace' => 'TCG\\Voyager\\Http\\Controllers',
+    ),
+    'models' => 
+    array (
+    ),
+    'assets_path' => '/vendor/tcg/voyager/assets',
+    'storage' => 
+    array (
+      'disk' => 'public',
+    ),
+    'hidden_files' => false,
+    'database' => 
+    array (
+      'tables' => 
+      array (
+        'hidden' => 
+        array (
+          0 => 'migrations',
+          1 => 'data_rows',
+          2 => 'data_types',
+          3 => 'menu_items',
+          4 => 'password_resets',
+          5 => 'permission_role',
+          6 => 'settings',
+        ),
+      ),
+    ),
+    'multilingual' => 
+    array (
+      'enabled' => false,
+      'rtl' => false,
+      'default' => 'en',
+      'locales' => 
+      array (
+        0 => 'en',
+      ),
+    ),
+    'dashboard' => 
+    array (
+      'navbar_items' => 
+      array (
+        'Profile' => 
+        array (
+          'route' => 'voyager.profile',
+          'classes' => 'class-full-of-rum',
+          'icon_class' => 'voyager-person',
+        ),
+        'Home' => 
+        array (
+          'route' => '/',
+          'icon_class' => 'voyager-home',
+          'target_blank' => true,
+        ),
+        'Logout' => 
+        array (
+          'route' => 'voyager.logout',
+          'icon_class' => 'voyager-power',
+        ),
+      ),
+      'widgets' => 
+      array (
+        0 => 'TCG\\Voyager\\Widgets\\UserDimmer',
+        1 => 'TCG\\Voyager\\Widgets\\PostDimmer',
+        2 => 'TCG\\Voyager\\Widgets\\PageDimmer',
+      ),
+    ),
+    'bread' => 
+    array (
+      'add_menu_item' => true,
+      'default_menu' => 'admin',
+      'add_permission' => true,
+      'default_role' => 'admin',
+    ),
+    'primary_color' => '#22A7F0',
+    'show_dev_tips' => true,
+    'additional_css' => 
+    array (
+    ),
+    'additional_js' => 
+    array (
+    ),
+    'googlemaps' => 
+    array (
+      'key' => '',
+      'center' => 
+      array (
+        'lat' => '32.715738',
+        'lng' => '-117.161084',
+      ),
+      'zoom' => 11,
+    ),
+  ),
+  'voyager-hooks' => 
+  array (
+    'enabled' => true,
+    'add-route' => true,
+    'add-hook-menu-item' => true,
+    'add-hook-permissions' => true,
+    'publish-vendor-files' => true,
+  ),
+  'laravel-widgets' => 
+  array (
+    'use_jquery_for_ajax_calls' => false,
+    'route_middleware' => 
+    array (
+      0 => 'web',
+    ),
+    'widget_stub' => 'vendor/arrilot/laravel-widgets/src/Console/stubs/widget.stub',
+    'widget_plain_stub' => 'vendor/arrilot/laravel-widgets/src/Console/stubs/widget_plain.stub',
+  ),
+  'laravellocalization' => 
+  array (
+    'supportedLocales' => 
+    array (
+      'en' => 
+      array (
+        'name' => 'English',
+        'script' => 'Latn',
+        'native' => 'English',
+        'regional' => 'en_GB',
+      ),
+      'es' => 
+      array (
+        'name' => 'Spanish',
+        'script' => 'Latn',
+        'native' => 'español',
+        'regional' => 'es_ES',
+      ),
+    ),
+    'useAcceptLanguageHeader' => true,
+    'hideDefaultLocaleInURL' => false,
+    'localesOrder' => 
+    array (
+    ),
+    'utf8suffix' => '.UTF-8',
+  ),
+  'trustedproxy' => 
+  array (
+    'proxies' => NULL,
+    'headers' => 30,
+  ),
+  'tinker' => 
+  array (
+    'dont_alias' => 
+    array (
+    ),
+  ),
+);
---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,101 @@
+<?php return array (
+  'arrilot/laravel-widgets' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Arrilot\\Widgets\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Widget' => 'Arrilot\\Widgets\\Facade',
+      'AsyncWidget' => 'Arrilot\\Widgets\\AsyncFacade',
+    ),
+  ),
+  'cartalyst/stripe-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Cartalyst\\Stripe\\Laravel\\StripeServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Stripe' => 'Cartalyst\\Stripe\\Laravel\\Facades\\Stripe',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'gloudemans/shoppingcart' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Gloudemans\\Shoppingcart\\ShoppingcartServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Cart' => 'Gloudemans\\Shoppingcart\\Facades\\Cart',
+    ),
+  ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+  'larapack/voyager-hooks' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Larapack\\VoyagerHooks\\VoyagerHooksServiceProvider',
+    ),
+  ),
+  'laravel/scout' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Scout\\ScoutServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'mcamara/laravel-localization' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Mcamara\\LaravelLocalization\\LaravelLocalizationServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'LaravelLocalization' => 'Mcamara\\LaravelLocalization\\Facades\\LaravelLocalization',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'tcg/voyager' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'TCG\\Voyager\\VoyagerServiceProvider',
+      1 => 'TCG\\Voyager\\Providers\\VoyagerDummyServiceProvider',
+    ),
+  ),
+);
